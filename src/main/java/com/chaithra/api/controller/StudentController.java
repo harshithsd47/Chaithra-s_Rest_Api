3ad585808d82154149e1cae0f2a1da55d6274bf9
Harshith <harshithsd47@gmail.com>
2025-04-26 08:38:30 +0530

    added the controller & the service class with 1 getMapping to find all the record from the database

---
+++ src/main/java/com/chaithra/api/controller/StudentController.java
@@ -0,0 +1,21 @@
+package com.chaithra.api.controller;
+
+import com.chaithra.api.entity.Student;
+import com.chaithra.api.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+}
