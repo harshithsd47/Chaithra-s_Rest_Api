cf2a72d541ed4afd727c7e7d1431ce9ca2a2599a
Harshith <harshithsd47@gmail.com>
2025-04-26 08:26:44 +0530

    created a repository package & added a student repository class

---
+++ src/main/java/com/chaithra/api/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+package com.chaithra.api.repository;
+
+import com.chaithra.api.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+
+
+
+
+
+}
